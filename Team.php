4ece32065984bd5405115b62fe67d13f17f31197
B00141403 Serena Emedeke <B00141403@mytudublin.ie>
2023-11-09 13:06:45 +0000

    part 2

--- Team.php
+++ Team.php
@@ -1,12 +1,12 @@
 <?php
-class Team 
+class Team
 {
     private $name;
     private $totalPoints = 0;
     private $totalGames = 0;
     private $totalGoals = 0;
-
-    public function __construct($name) 
+}
+    public function __construct($name)
     {
         $this->name = $name;
     }
@@ -15,11 +15,26 @@ class Team
     {
         $this->totalGames++;
         $this->totalGoals += $homeTeamScore;
-    }
+
         if ($homeTeamScore > $opposingTeamScore) {
             $this->totalPoints += 3;
         } elseif ($homeTeamScore == $opposingTeamScore) {
             $this->totalPoints += 1;
         }
+    }
+    }
+        public function getGoalAverage()      
+    }
+       
+            if ($this->totalGames == 0)  {
+            return 0.0;
+            }
+            return $this->totalGoals / $this->totalGames;
+   
+   
+    }
 
- 
+}
+
+       
+?>
--- teamTester.php
+++ teamTester.php
@@ -4,9 +4,11 @@ include("Team.php");
 
 $myTeam = new Team("Tigers");
 
+$myTeam->finalScore(3,2);
 $myTeam->finalScore(2,2);
+$myTeam->finalScore(1,0);
 
-echo "That worked";
+echo $myTeam->getGoalAverage();
 
 
 ?>
